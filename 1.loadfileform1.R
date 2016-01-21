d1909be63316dd758dbbab8e4998d01fe28ee98e
Sith Jaisong <sith.jaisong@gmail.com>
2016-01-21 14:59:08 +0800

    :nada: :nada: Finish :tada: :tada:
    add
    1.loadfileform1.R
    2.loadfileform2.R
    3.combine1_2.R
    with functions for extract data from the excel format to data frame
    format

---
+++ 1.loadfileform1.R
@@ -0,0 +1,92 @@
+##############################################################################
+# title         : 1.loadfileform1.R;
+# purpose       : extract the general information from the form1 in excel file;
+# producer      : prepared by S. jaiosong;
+# last update   : in Los Banos, IRRI, 21 January 2016;
+# inputs        : crop health survey form 1 (excel);
+# outputs       : general information of survey data of SKEP Phase I from all locations;
+##############################################################################
+
+#=====Load library =====
+library(XLConnect)
+library(dplyr)
+
+# ==== Load function
+# change following your folder
+source("~/Documents/Github/surveySKEP1/Functions/form1.R")
+
+# set working directory 
+options(stringsAsFactors = FALSE) 
+#setwd('~/Google Drive/survey1')
+#files can be downloaded form the Google Drive that I shared
+# I will create the script for make the lists of the files of the survey excel file
+# setwd('~/Google Drive/survey1') # this is for Mac
+setwd("~/Desktop/surveySKEP1")
+# setwd('E:/Google Drive/surveySKEP1') # this is for Window
+
+# list the folders, there are 6 sub-folders
+dirlist <- list.dirs()
+# [2] IDN#85 
+# [3] IND#85 
+# [4] PHL#40 
+# [5] #THA#105 
+# [6] VNM#70
+
+#==== Start extrating here ====#
+
+FORM1  <- data.frame() 
+
+for (j in 2:length(dirlist)){
+  list.file <- list.files(dirlist[j], pattern = ".xls")
+  
+for (i in 1:length(list.file)) {
+    
+    dataf1 <- readWorksheet(loadWorkbook(paste(dirlist[j], "/", list.file[i], sep = "")), sheet = 1, startCol = 1, endCol = 30, 
+        startRow = 1, endRow = 100, autofitRow = FALSE, autofitCol = FALSE)
+    
+    new.row <- as.data.frame(form1(dataf1)) # store the extracted data
+    
+    new.row$filename <- as.character(list.file[i]) # inset the column named "filename"
+    
+    # corract the type of variables
+    new.row <- new.row %>% transform(
+      filename = as.character(as.factor(filename)),
+      field.area = as.numeric(as.character(field.area)),
+      farmer.name = as.character(farmer.name),
+      land.form = as.character(land.form),
+      prev.crop = as.character(prev.crop),
+      fallow.prd = as.numeric(as.character(fallow.prd)),
+      soil.salt = as.character(soil.salt),
+      soil.zinc = as.character(soil.salt),
+      soil.alum = as.character(soil.alum),
+      crop.est.method = as.character(crop.est.method),
+      seed.age = as.numeric(as.character(seed.age)),
+      rice.var = as.character(rice.var),
+      traditional= as.character(traditional),
+      modern = as.character(modern),
+      hybrid = as.character(hybrid),
+      Ntotal = as.numeric(as.character(Ntotal)),
+      Ptotal = as.numeric(as.character(Ptotal)),
+      Ktotal = as.numeric(as.character(Ktotal)),
+      yld.area1 = as.numeric(as.character(yld.area1)),
+      yld.area2 = as.numeric(as.character(yld.area2)),
+      yld.area3 = as.numeric(as.character(yld.area3))
+      )
+    
+    FORM1 <- rbind(FORM1, new.row)
+}
+}
+
+# remove the row name
+row.names(FORM1) <- NULL
+
+# move the filename column up to the front column
+col_idx <- grep("filename", names(FORM1))
+FORM1 <- FORM1[, c(col_idx, (1:ncol(FORM1))[-col_idx])]
+
+# FORM1 data is in the right data frame
+#FORM1 <- data.frame(lapply(FORM1, as.character), stringsAsFactors = FALSE)
+
+#write.csv(dataF1, file = "dataf1.vnm.csv")
+
+ 
---
+++ 2.loadfileform2.R
@@ -0,0 +1,142 @@
+##############################################################################
+# title         : 2.loadfileform2.R;
+# purpose       : extract the injuries and diseases from the form2visit1 and form2visit2 in excel file;
+# producer      : prepared by S. jaiosong;
+# last update   : in Los Banos, IRRI, 21 January 2016;
+# inputs        : crop health survey form 2 (excel);
+# outputs       : injuries and disease data of SKEP Phase I from all locations;
+##############################################################################
+# ==== Load libraries
+library(XLConnect)
+library(dplyr)
+library(tidyr)
+library(reshape)
+library(reshape2)
+
+# Load function
+source("~/Documents/Github/surveySKEP1/Functions/injury.R")
+source("~/Documents/Github/surveySKEP1/Functions/tidy_injury.R")
+
+source("~/Documents/Github/surveySKEP1/Functions/weed.R")
+source("~/Documents/Github/surveySKEP1/Functions/tidy_weed.R")
+
+setwd("~/Desktop/surveySKEP1")
+# load data of form2
+
+dirlist <- list.dirs()
+# [2] IDN#85 
+# [3] IND#85 
+# [4] PHL#40 
+# [5] THA#105 
+# [6] VNM#70
+
+form2.injury <- matrix(nrow = 0, ncol = 31)  # crearte the object to store the injury data
+form2.weed <- matrix(nrow = 0, ncol = 10)  # create the object to store the weed data
+
+for (j in 2:length(dirlist)) {
+    list.file <- list.files(dirlist[j], pattern = ".xls")
+    
+    for (i in 1:length(list.file)) {
+        # read sheet3 'form2 visit1'
+        dataf2.1 <- readWorksheet(loadWorkbook(paste(dirlist[j], "/", list.file[i], sep = "")), sheet = 2, startCol = 1, 
+            endCol = 15, startRow = 1, endRow = 70, autofitRow = FALSE, autofitCol = FALSE)
+        # read sheet3 'form2 visit2'
+        dataf2.2 <- readWorksheet(loadWorkbook(paste(dirlist[j], "/", list.file[i], sep = "")), sheet = 3, startCol = 1, 
+            endCol = 15, startRow = 1, endRow = 70, autofitRow = FALSE, autofitCol = FALSE)
+            
+            # ==== visit1.injury=====
+            visit1.injury <- as.data.frame(injury(dataf2.1))
+            visit1.injury$visit <- "1"
+            visit1.injury$filename <- as.character(list.file[i])  # change the list name
+            visit1.injury$DVS <- dataf2.1[3, 4]
+            
+            # arrange the column by moving the filename to the first, and visit and DVS inthe second and third, respectively.
+            col_idx <- grep("DVS", names(visit1.injury))
+            visit1.injury <- visit1.injury[, c(col_idx, (1:ncol(visit1.injury))[-col_idx])]
+            
+            col_idx <- grep("visit", names(visit1.injury))
+            visit1.injury <- visit1.injury[, c(col_idx, (1:ncol(visit1.injury))[-col_idx])]
+            
+            col_idx <- grep("filename", names(visit1.injury))
+            visit1.injury <- visit1.injury[, c(col_idx, (1:ncol(visit1.injury))[-col_idx])]
+            
+            visit1.injury <- tidy_injury(visit1.injury)
+            
+            # ====visit2.injury=====
+            visit2.injury <- as.data.frame(injury(dataf2.2))
+            visit2.injury$filename <- as.character(list.file[i])  # change the list name
+            visit2.injury$visit <- "2"
+            visit2.injury$DVS <- dataf2.2[3, 4]
+            
+            # arrange the column by moving the filename to the first, and visit and DVS inthe second and third, respectively.
+            col_idx <- grep("DVS", names(visit2.injury))
+            visit2.injury <- visit2.injury[, c(col_idx, (1:ncol(visit2.injury))[-col_idx])]
+            
+            col_idx <- grep("visit", names(visit2.injury))
+            visit2.injury <- visit2.injury[, c(col_idx, (1:ncol(visit2.injury))[-col_idx])]
+            
+            col_idx <- grep("filename", names(visit2.injury))
+            visit2.injury <- visit2.injury[, c(col_idx, (1:ncol(visit2.injury))[-col_idx])]
+            
+            visit2.injury <- tidy_injury(visit2.injury)
+            
+            # === combind visit1 and visit2 injury=====
+            new.row.injury <- rbind(visit1.injury, visit2.injury)
+            form2.injury <- rbind(form2.injury, new.row.injury)
+            
+            
+            # === vist1.weed ======
+            visit1.weed <- as.data.frame(weed(dataf2.1))
+            visit1.weed$visit <- "1"
+            visit1.weed$filename <- as.character(list.file[i])  # change the list name
+            visit1.weed$DVS <- dataf2.1[3, 4]
+            
+            col_idx <- grep("DVS", names(visit1.weed))
+            visit1.weed <- visit1.weed[, c(col_idx, (1:ncol(visit1.weed))[-col_idx])]
+            
+            col_idx <- grep("visit", names(visit1.weed))
+            visit1.weed <- visit1.weed[, c(col_idx, (1:ncol(visit1.weed))[-col_idx])]
+            
+            col_idx <- grep("filename", names(visit1.weed))
+            visit1.weed <- visit1.weed[, c(col_idx, (1:ncol(visit1.weed))[-col_idx])]
+            
+            visit1.weed <- tidy_weed(visit1.weed)
+            
+            # ==== visit2.weed====
+            
+            visit2.weed <- as.data.frame(weed(dataf2.2))
+            visit2.weed$visit <- "2"
+            visit2.weed$filename <- as.character(list.file[i])  # change the list name
+            visit2.weed$DVS <- dataf2.2[3, 4]
+            
+            col_idx <- grep("DVS", names(visit2.weed))
+            visit2.weed <- visit2.weed[, c(col_idx, (1:ncol(visit2.weed))[-col_idx])]
+            
+            col_idx <- grep("visit", names(visit2.weed))
+            visit2.weed <- visit2.weed[, c(col_idx, (1:ncol(visit2.weed))[-col_idx])]
+            
+            col_idx <- grep("filename", names(visit2.weed))
+            visit2.weed <- visit2.weed[, c(col_idx, (1:ncol(visit2.weed))[-col_idx])]
+            
+            visit2.weed <- tidy_weed(visit2.weed)
+            
+            # ====combine visit1 and visit 2 weed
+            
+            new.row.weed <- rbind(visit1.weed, visit2.weed)
+            
+            form2.weed <- rbind(form2.weed, new.row.weed)
+            
+            # ====combind weed and injury data
+            
+            FORM2 <- merge(form2.weed, form2.injury)
+        }
+    }
+
+
+# output is new.row.sum
+FORM2  # the final output
+
+# save the output write.csv(FORM2, 'FORM2.csv')
+
+
+ 
