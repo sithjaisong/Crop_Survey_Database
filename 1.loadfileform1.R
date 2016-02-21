5116aabfdd6b5a4d4f1127fc0de9d36048e2f12d
Sith Jaisong <sith.jaisong@gmail.com>
2016-02-21 20:20:50 +0800

    [1.loadfileform1] add comments

--- 1.loadfileform1.R
+++ 1.loadfileform1.R
@@ -20,40 +20,58 @@ setwd("~/Google Drive/surveySKEP1")
 
 # list the folders, there are 6 sub-folders
 dirlist <- list.dirs()
-# [2] IDN#85 [3] IND#85 [4] PHL#40 [5] THA#105 [6] VNM#70 ==== Start
-# extrating here ====#
-
 
 # ===== Load Form1 data from all file in this working directory ==== #
 
-source("~/Documents/Github/Crop_Survey_Database/Functions/form1_df.R") # call function readForm1
+# call function readForm1
+source("~/Documents/Github/Crop_Survey_Database/Functions/form1_df.R") 
 
+# create FORM1 to keep the data
 FORM1 <- data.frame()
 
 for (j in 2:length(dirlist)) {
-
+  # create list.file of files which have names end with .xls from each country folder
   list.file <- list.files(dirlist[j], pattern = ".xls")
 
   for (i in 1:length(list.file)) {
-
+  # extract the form1 data from each xls
+  # return wide table formate for Form1 data in the dataframe
     dataf1 <- readWorksheet(loadWorkbook(paste(dirlist[j], "/", list.file[i],
                                                sep = "")), sheet = 1, startCol = 1, endCol = 30, startRow = 1,
                             endRow = 100, autofitRow = FALSE, autofitCol = FALSE)
 
-    # = extract Form1 data from excel
-    # = return wide table formate for Form1 data in the dataframe
-    # = reshape from wide table data frame to long table format
-
+    # reshape from wide table data frame to long table format
     new.field <- as.data.frame(form1_df(dataf1))
 
-    # = inset the column named 'filename'
-
-    new.field$index <- as.character(list.file[i])  #
-
+    # inset the column named 'filename'
+    new.field$index <- as.character(list.file[i]) 
+  
+    # reoorder column by setting first column is index, whcih is filenamme
     new.field <- new.field %>% select(index, everything())
 
+    # insert new row
     FORM1 <- rbind(FORM1, new.field)
-
   }
 }
+
+# save filename
+filename <- FORM1$index
+
+# seperate contry data and season data from filename
+FORM1 <- FORM1 %>% separate(index, c("SKEP", "SURVEY", "Country", "Year",
+                                     "Season", "Fieldno"), "-") %>% transform(Fieldno = gsub(pattern = "\\.xls$",
+                                                                                             replacement = "", .$Fieldno))
+# delete column named SKEP and SURVEY
+FORM1$SKEP <- NULL
+FORM1$SURVEY <- NULL
+
+# re-add the index column 
+FORM1$index <- filename
+
+# reorder column named index in the first column
+FORM1 <- FORM1 %>% select(index, everything())
+
+# delete row name
+row.names(FORM1) <- NULL
+
 #eos
