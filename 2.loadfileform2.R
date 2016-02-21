4d183eb9b8913522856074860db320384e63e04a
Sith Jaisong <sith.jaisong@gmail.com>
2016-02-21 20:22:41 +0800

    [2.loadfileform2] move functions to function combine1_2
    move sources of wide and long .R to combine1_2 because it dose not need
    to show in this

--- 2.loadfileform2.R
+++ 2.loadfileform2.R
@@ -20,29 +20,23 @@ setwd("~/Google Drive/surveySKEP1")
 
 # list the folders, there are 6 sub-folders
 dirlist <- list.dirs()
-# [2] IDN#85 [3] IND#85 [4] PHL#40 [5] THA#105 [6] VNM#70 ==== Start
-# extrating here ====#
-
 
 # ===== Load Form2 data from all file in this working directory ==== #
 
-
-# Load function
-source("~/Documents/Github/Crop_Survey_Database/Functions/wide_injury.R")
-source("~/Documents/Github/Crop_Survey_Database/Functions/long_injury.R")
+#load function combine)injury1_2
 source("~/Documents/Github/Crop_Survey_Database/Functions/combind_injury1_2.R")
 
-source("~/Documents/Github/Crop_Survey_Database/Functions/wide_weed.R")
-source("~/Documents/Github/Crop_Survey_Database/Functions/long_weed.R")
+#load function combine_weed1_2
 source("~/Documents/Github/Crop_Survey_Database/Functions/combind_weed1_2.R")
 
-source("~/Documents/Github/Crop_Survey_Database/Functions/wide_sweep.R")
-source("~/Documents/Github/Crop_Survey_Database/Functions/long_sweep.R")
+#load function combine_sweep1_2
 source("~/Documents/Github/Crop_Survey_Database/Functions/combind_sweep1_2.R")
 
+# create FORM2 dataframe for keep the data
 FORM2 <- data.frame()
 
 for (j in 2:length(dirlist)) {
+  # keep the file ended with .xls 
   list.file <- list.files(dirlist[j], pattern = ".xls")
   
   for (i in 1:length(list.file)) {
@@ -56,18 +50,21 @@ for (j in 2:length(dirlist)) {
     dataf2.2 <- readWorksheet(loadWorkbook(paste(dirlist[j], "/", list.file[i],
                                                  sep = "")), sheet = 3, startCol = 1, endCol = 15, startRow = 1,
                               endRow = 70, autofitRow = FALSE, autofitCol = FALSE)
-    check.df2.1 <- dataf2.1[10, 5]  # checking the leaf data, which is indicated whether or not this sheet 'form2visit1' is filled up.
-    check.df2.2 <- dataf2.2[10, 5]  # checking the leaf data, which is indicated whether or not this sheet 'form2visit2' is filled up.
     
+    # checking the leaf data, which is indicated whether or not this sheet 'form2visit1' is filled up.
+    check.df2.1 <- dataf2.1[10, 5]  
+    # checking the leaf data, which is indicated whether or not this sheet 'form2visit2' is filled up.
+    check.df2.2 <- dataf2.2[10, 5]  
     
+    # if those data have not the data that mean that sheet does not have data
     if (is.na(check.df2.1) == TRUE | is.na(check.df2.2) == TRUE) {
-      
+      # then will report which files have not the data, and will skip this file
       print(paste(list.file[i], "is not complete because either visit1 or visit2 has not data",
                   sep = " "))
-      next  # mean run
-      # print('RUN')
+      next  
     } else {
       
+      # combine form2 data
       injury <- combind_injury1_2(dataf2.1, dataf2.2)
       
       injury$filename <- as.character(list.file[i])
@@ -87,8 +84,7 @@ for (j in 2:length(dirlist)) {
       
       form2.list <- list(weed, injury, sweep)
       
-      new.row.injury <- merge_recurse(form2.list, by = c("filename", "visit",
-                                                         "DVS"))
+      new.row.injury <- merge_recurse(form2.list, by = c("filename", "visit", "DVS"))
       
       FORM2 <- rbind(FORM2, new.row.injury)
     }
